903be955952c2031801b977220c3dbf1a3b509bb
Zezhe Huang <kiki_hzz@163.com>
2021-12-12 23:08:35 +0100

    Blog Model

--- session-2/myblog/blogs/models.py
+++ session-2/myblog/blogs/models.py
@@ -1,3 +1,10 @@
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    title = models.TextField()
+    content = models.TextField()
+
