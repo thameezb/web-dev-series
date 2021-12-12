ac8d360b21e21e3d9053224cbfb006e8029b398d
Zezhe Huang <kiki_hzz@163.com>
2021-12-13 00:29:58 +0100

    Restful API

--- session-2/myblog/blogs/views.py
+++ session-2/myblog/blogs/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from rest_framework import serializers, viewsets, permissions
+from .models import Blog
 
-# Create your views here.
+class BlogSerializer(serializers.ModelSerializer):
+    author = serializers.PrimaryKeyRelatedField(read_only=True)
+    class Meta:
+        model = Blog
+        fields = '__all__'
+    
+    def create(self, validated_data):
+        request = self.context['request']
+        user = getattr(request, 'user', None)
+        if not user.is_authenticated:
+            raise serializers.ValidationError('Only authenticated users are allowed to create blogs.')
+        validated_data['author'] = user
+        return Blog.objects.create(**validated_data)
+
+
+class AuthorPermission(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.author == request.user
+
+class BlogViewSet(viewsets.ModelViewSet):
+    queryset = Blog.objects.all()
+    serializer_class = BlogSerializer
+    permission_classes = [AuthorPermission, ]
--- session-2/myblog/myblog/urls.py
+++ session-2/myblog/myblog/urls.py
@@ -14,14 +14,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from rest_framework import routers
+from blogs.views import BlogViewSet
+
+router = routers.SimpleRouter()
+router.register(r'blogs', BlogViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('', include(router.urls)),
 ]
