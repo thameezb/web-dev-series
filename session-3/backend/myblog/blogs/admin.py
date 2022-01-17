ca45c2d503b9e171483e2a58230ef4a7eddd2919
ThameezBo <thameez.bodhanya@allangray.co.za>
2022-01-17 14:00:28 +0100

    fix: copy backend code

---
+++ session-3/backend/myblog/blogs/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from blogs.models import Blog
+
+class BlogAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Blog, BlogAdmin)
---
+++ session-3/backend/myblog/blogs/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from rest_framework import serializers, viewsets, permissions
+from .models import Blog
+
+class BlogSerializer(serializers.ModelSerializer):
+    author = serializers.PrimaryKeyRelatedField(read_only=True)
+    author_username = serializers.CharField(source='author.username', read_only=True)
+    class Meta:
+        model = Blog
+        fields = '__all__'
+    
+    def create(self, validated_data):
+        request = self.context['request']
+        user = getattr(request, 'user', None)
+        if not user.is_authenticated:
+            raise serializers.ValidationError('Only authenticated users are allowed to create blogs.')
+        validated_data['author'] = user
+        return Blog.objects.create(**validated_data)
+
+
+class AuthorPermission(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.author == request.user
+
+class BlogViewSet(viewsets.ModelViewSet):
+    queryset = Blog.objects.all()
+    serializer_class = BlogSerializer
+    permission_classes = [AuthorPermission, ]
