590736258fef7ca5167d44e38a0cda5c22f9579f
ThameezBo <thameez.bodhanya@allangray.co.za>
2022-01-23 09:01:20 +0100

    feat: navbar

--- session-3/frontend/src/App.jsx
+++ session-3/frontend/src/App.jsx
@@ -1,5 +1,7 @@
+import Navbar from './components/NavBar';
 import Login from './components/Login/Login';
 import useToken from './components/useToken';
+import { BrowserRouter as Router} from 'react-router-dom'; 
 import React from 'react';
 
 function App() {
@@ -10,22 +12,10 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar setToken={setToken}/>
+      </div>
   );
 }
 
